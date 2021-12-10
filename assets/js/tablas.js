2515e9a6115c3259077efeda668b3df9c5b9e33b
Jesus Campos <jesus.campos123@icloud.com>
2021-12-09 23:03:23 -0500

    Añadiendo últimos detalles

--- assets/js/tablas.js
+++ assets/js/tablas.js
@@ -54,5 +54,58 @@ const clientextipo = () => {
 	})
 }
 
+const cliente_asesor = () => {
+	const labels = [
+		"3577",
+		"3325",
+		"7904", 
+		"8354",
+		"4448",
+		"3789",
+		"3966",
+		"5955",
+		"2008",
+		"5922"
+	]
+
+	const valores = []
+	for (let i = 0; i < labels.length; i++) {
+		const cant_clientes = document.getElementById('ase_' + labels[i]).value
+		valores.push(cant_clientes)
+	}
+
+	console.log(labels)
+
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: '',
+				data: valores,
+				backgroundColor: [
+					"#1d3e6e",
+					"#7a95b0",
+					"#8db046",
+					"#fe3533",
+					"##900001",
+					"#a6b401",
+					"#eff67b",
+					"#d50102",
+					"#6c0102",
+					"b3e7dc"
+				]
+			}
+		]
+	}
+
+	const cliente_asesor = document.getElementById("cliente_asesor").getContext('2d')
+
+	const chart = new Chart(cliente_asesor, {
+		type: 'bar',
+		data: data
+	})
+}
+
 window.addEventListener('load', articuloxdonde)
 window.addEventListener('load', clientextipo)
+window.addEventListener('load', cliente_asesor)
--- routes/analisis.js
+++ routes/analisis.js
@@ -19,6 +19,8 @@ const ARTICULOxDONDE = async (cod) => {
 	return cant[0]
 }
 
+//////////////////////////////////////////////////////////////////////////////////
+
 const CLIENTExTIPO = async (cod) => {
 	const cant = []
 	const query = `select
@@ -36,6 +38,47 @@ const CLIENTExTIPO = async (cod) => {
 	return cant[0]
 }
 
+//////////////////////////////////////////////////////////////////////////////////
+
+const obtener_asesores = async () => {
+	const asesores = []
+	const query = 'select * from asesor'
+	const result = await DB.Open(query, [], false)
+
+	result.rows.map(e => {
+		const user_schema = {
+			id_ase: e[0],
+			nombre: e[1],
+			apellido: e[2],
+			telefono: e[3],
+			correo: e[4],
+			fecha_ingreso: e[5]
+		}
+		asesores.push(user_schema)
+	})
+
+	return asesores
+}
+
+const CLIENTE_ASESOR = async (cod) => {
+	const cant = []
+	const query = `select
+						CLIENTE_ASESOR(${cod})
+					from dual`
+	const result = await DB.Open(query, [], false)
+
+	result.rows.map(e => {
+		const user_schema = {
+			cant: e[0]
+		}
+		cant.push(user_schema.cant)
+	})
+
+	return cant[0]
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
 router.get('/analisis', async (req, res) => {
 	const nacional = await ARTICULOxDONDE(1)
 	const extranjero = await ARTICULOxDONDE(2)
@@ -43,11 +86,18 @@ router.get('/analisis', async (req, res) => {
 	const p_jur = await CLIENTExTIPO(1)
 	const p_nat = await CLIENTExTIPO(2)
 
+	const asesores = await obtener_asesores()
+	for (let asesor of asesores) {
+		asesor.cant_clientes = await CLIENTE_ASESOR(asesor.id_ase)
+	}
+
+	console.log(asesores)
 	res.render('analisis', {
 		nacional: nacional,
 		extranjero: extranjero,
 		p_jur: p_jur,
-		p_nat: p_nat
+		p_nat: p_nat,
+		asesores: asesores
 	})
 })
 
